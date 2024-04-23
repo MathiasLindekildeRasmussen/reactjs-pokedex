a593dc90bb289276fd3e4831e87b109769f1dce7
MathiasLindekildeRasmussen <mathias.lindekilde@gmail.com>
2024-04-23 14:01:15 +0200

    awd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // Home component to include what was previously directly in App.js
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Home from './components/home';
 import PokemonDetails from './components/pokemondetails';
